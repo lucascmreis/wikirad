d67d6448e1537c01fe44faa35ad28f1dc13dec97
lucascmreis <reislcr.rj@gmail.com>
2020-11-12 11:03:18 -0300

    add logo and banner

---
+++ src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// Path to the logo file on your project
+import wikiradLogo from '../../../assets/wikiradLogo.png';
+
+const Logo = () => (
+  <img src={wikiradLogo} alt="Wikirad logo" style={{ width: 180 }} />
+);
+
+export default Logo;
---
+++ src/@rocketseat/gatsby-theme-docs/styles/theme.js
@@ -0,0 +1,9 @@
+export default {
+    colors: {
+      primary: '#0956ba',
+      background: '#FFFFFF',
+      shape: `#F2F2FA`,
+      title: `#3D3D4D`,
+      text: `#012350`,
+    },
+  };
