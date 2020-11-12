9d144d9da022d24914ca496347d0dbb9e62b6533
lucascmreis <reislcr.rj@gmail.com>
2020-11-12 13:16:25 -0300

    Github edited

---
+++ src/@rocketseat/gatsby-theme-docs/components/Docs/EditGithub.js
@@ -0,0 +1,41 @@
+/* @jsx jsx */
+import { jsx, css } from '@emotion/core';
+import PropTypes from 'prop-types';
+import { MdEdit } from 'react-icons/md';
+import { useTheme } from 'emotion-theming';
+
+export default function EditGithub({ githubEditUrl }) {
+  const theme = useTheme();
+
+  if (githubEditUrl) {
+    return (
+      <a
+        href={githubEditUrl}
+        target="_blank"
+        rel="noopener noreferrer"
+        css={css`
+          display: flex;
+          align-items: center;
+          text-decoration: none;
+          margin-top: 48px;
+          color: ${theme.colors.text};
+          opacity: 0.8;
+          font-size: 14px;
+          font-weight: normal;
+        `}
+      >
+        <MdEdit style={{ marginRight: '5px' }} />
+        Editar no Github
+      </a>
+    );
+  }
+  return null;
+}
+
+EditGithub.propTypes = {
+  githubEditUrl: PropTypes.string,
+};
+
+EditGithub.defaultProps = {
+  githubEditUrl: null,
+};
